1c014365393916a9a784927ba4957ad7a968c888
meggl <maximilianfeggl@gmail.com>
2023-06-13 17:08:19 +0200

    Version change

--- App/to_pyqt.py
+++ App/to_pyqt.py
@@ -2147,7 +2147,7 @@ class MainWindow(QWidget):
         # headline
         self.headline = QLabel(self)
         self.headline.setTextFormat(Qt.TextFormat.RichText)
-        self.headline.setText("The Dendritic Spine Tool <br> <font size='0.1'>v0.5.0-alpha</font>")
+        self.headline.setText("The Dendritic Spine Tool <br> <font size='0.1'>v0.5.1-alpha</font>")
         Font = QFont("Courier", 60)
         self.headline.setFont(Font)
         self.headline.setStyleSheet("color: white")
