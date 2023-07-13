Corrected the calculation of IntDen in the synapse IntDen calculation.

App/Dendrite.py
@@ -174,7 +174,7 @@ class Dendrite:
                 for j in range(Chans):
                     self.dend_lumin[pdx,i,j] = self.tiff_arr[i,j,p[1],p[0]]
 
-        gaussian_mask = (gaussian_filter(input=mask, sigma=4) >= np.mean(mask)).astype(np.uint8)
+        gaussian_mask = (gaussian_filter(input=mask, sigma=sigma) >= np.mean(mask)).astype(np.uint8)
         self.contours, _ = cv.findContours(gaussian_mask, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
         self.dendritic_surface_matrix= gaussian_mask
 

App/Utility.py
@@ -186,7 +186,7 @@ def getWidthnew(img, all_ps, sigma, max_neighbours, width_factor: int=1):
     """
 
 
-    edges1 = feature.canny(img, sigma=3)
+    edges1 = feature.canny(img, sigma=sigma)
 
 
     i_img = np.zeros(img.shape)