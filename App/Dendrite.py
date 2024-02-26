v8.6, added masks/imagej for area calculation + attempted to fix csv width of dendrite

App/Dendrite.py
@@ -172,10 +172,9 @@ class Dendrite:
         for pdx, p in enumerate(self.smoothed_all_pts):
             self.dend_stat[pdx, 0] = p[1]
             self.dend_stat[pdx, 1] = p[0]
-            self.dend_stat[pdx, 2] = width_arr[pdx]/width_factor
+            self.dend_stat[pdx, 2] = width_arr[pdx]
             self.dend_stat[pdx, 3] = 2
             self.dend_stat[pdx, 4] = degrees[pdx]
-
             mask = self.GenEllipse(mask,p,pdx,degrees,width_arr,Snaps,Chans)
 
         gaussian_mask = (gaussian_filter(input=mask, sigma=sigma) >= np.mean(mask)).astype(np.uint8)
@@ -369,7 +368,7 @@ def DendSave_csv(Dir,Dend_Arr,shift):
     nSnaps = Dend_Arr[0].dend_lumin.shape[-2]
     customhead = ['Dendrite']
 
-    oglist = [['Dendrite: '+str(i)]+['Width'] +['Timestep '+ str(i) + ' (Luminosity (mid.))' for i in range(1,nSnaps+1)]
+    oglist = [['Dendrite: '+str(i)]+['Width of ell.'] +['Timestep '+ str(i) + ' (Luminosity (mid.))' for i in range(1,nSnaps+1)]
      +['Timestep '+ str(i) + ' (Luminosity (ell.))' for i in range(1,nSnaps+1)] for i in range(len(Dend_Arr))]
     flattened_list = [item for sublist in oglist for item in sublist]
     for c in range(nChans):
@@ -378,7 +377,7 @@ def DendSave_csv(Dir,Dend_Arr,shift):
         for D in Dend_Arr:
             pts   = D.dend_stat[:,:2] + shift
             loc   = np.array([str([x,y]) for x,y in pts])
-            width = np.array([str(t*D.WidthFactor) for t in pts])
+            width = np.array(D.dend_stat[:,2])
             x =  np.hstack([loc.reshape(-1,1),width.reshape(-1,1),D.dend_lumin[:,:,c],D.dend_lumin_ell[:,:,c]])
             DendVar.append(x)
         max_sublists = max(len(var) for var in DendVar)

App/to_pyqt.py
@@ -1108,6 +1108,7 @@ class DataReadWindow(QWidget):
             SaveFlag[0] = False
 
         if(len(self.SpineArr)>0):
+            self.SaveROIstoSpine()
             if(not self.SpinesMeasured):
                 self.spine_measure()
                 self.SpinesMeasured = True
@@ -1133,21 +1134,39 @@ class DataReadWindow(QWidget):
 
                 orderedSpineArr = [self.SpineArr[t] for t in T]
                 #save spine masks
-                for i,t in  enumerate(T):
-                    R = self.roi_interactor_list[t]
-                    Spine_Mask_Dir = Spine_Dir + "Mask_" + str(i) + ".png"
-                    xperts = R.getPolyXYs()
-                    mask = np.zeros_like(self.tiff_Arr[0,0])
 
-                    c = np.clip(xperts[:, 0],0,self.tiff_Arr.shape[-1]-1)
-                    r = np.clip(xperts[:, 1],0,self.tiff_Arr.shape[-2]-1)
-                    rr, cc = polygon(r, c)
-                    mask[rr, cc] = 255
-                    try:
-                        mask = np.pad(mask,((-self.SimVars.xLims[0],self.SimVars.xLims[1]),(-self.SimVars.yLims[0],self.SimVars.yLims[1])),'constant', constant_values=(0,0))
-                    except:
-                        pass
-                    cv.imwrite(Spine_Mask_Dir, mask)
+                for i,t in  enumerate(T):
+                    if(self.SimVars.Mode=="Luminosity" or not self.SimVars.multitime_flag):
+                        R = self.roi_interactor_list[t]
+                        Spine_Mask_Dir = Spine_Dir + "Mask_" + str(i) + ".png"
+                        xperts = R.getPolyXYs()
+                        mask = np.zeros_like(self.tiff_Arr[0,0])
+
+                        c = np.clip(xperts[:, 0],0,self.tiff_Arr.shape[-1]-1)
+                        r = np.clip(xperts[:, 1],0,self.tiff_Arr.shape[-2]-1)
+                        rr, cc = polygon(r, c)
+                        mask[rr, cc] = 255
+                        try:
+                            mask = np.pad(mask,((-self.SimVars.xLims[0],self.SimVars.xLims[1]),(-self.SimVars.yLims[0],self.SimVars.yLims[1])),'constant', constant_values=(0,0))
+                        except:
+                            pass
+                        cv.imwrite(Spine_Mask_Dir, mask)
+                    else:
+                        for j,xperts in enumerate(self.SpineArr[t].points):
+                            xperts = np.array(xperts)
+                            Spine_Mask_Dir = Spine_Dir + "Mask_" + str(i) +"_t"+str(j)+ ".png"
+                            mask = np.zeros_like(self.tiff_Arr[0,0])
+
+                            c = np.clip(xperts[:, 0],0,self.tiff_Arr.shape[-1]-1)
+                            r = np.clip(xperts[:, 1],0,self.tiff_Arr.shape[-2]-1)
+                            rr, cc = polygon(r, c)
+                            mask[rr, cc] = 255
+                            try:
+                                mask = np.pad(mask,((-self.SimVars.xLims[0],self.SimVars.xLims[1]),(-self.SimVars.yLims[0],self.SimVars.yLims[1])),'constant', constant_values=(0,0))
+                            except:
+                                    pass
+                            cv.imwrite(Spine_Mask_Dir, mask)
+
                 nSnaps = self.number_timesteps if self.SimVars.multitime_flag else 1
                 nChans = self.number_channels if self.SimVars.multiwindow_flag else 1
                 try:
@@ -2745,7 +2764,7 @@ class MainWindow(QWidget):
         # headline
         self.headline = QLabel(self)
         self.headline.setTextFormat(Qt.TextFormat.RichText)
-        self.headline.setText("The Dendritic Spine Tool <br> <font size='0.1'>v0.8.5-alpha</font>")
+        self.headline.setText("The Dendritic Spine Tool <br> <font size='0.1'>v0.8.6-alpha</font>")
         Font = QFont("Courier", 60)
         self.headline.setFont(Font)
         self.headline.setStyleSheet("color: white")