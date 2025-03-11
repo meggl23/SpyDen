Finally fixed width calculation stuff + bug fixes + implementation of soma (no caluclation of neck)

App/DataRead.py
@@ -11,6 +11,7 @@ from .Spine    import *
 from .Dendrite import *
 from .PathFinding import GetLength
 from .SynapseFuncs import *
+from .MPL_Widget import *
 
 import json
 
@@ -442,7 +443,7 @@ def Measure(SynArr, tiff_Arr, SimVars,frame=None):
     else:
         Chans = 1
     if(Chans>1):
-        if(SimVars.Mode=="Luminosity" or Snaps==1):
+        if(SimVars.Mode=="Luminosity"):
             for S in SynArr:
                 frame.set_status_message.setText(frame.set_status_message.text()+'.')
                 for i in range(SimVars.Channels):
@@ -454,21 +455,22 @@ def Measure(SynArr, tiff_Arr, SimVars,frame=None):
                     S.mean.append(Mean)
                     S.local_bg.append(local_bg)
                 S.area.append(Area[0])
-                SpineBoundingBox(S,SimVars.Unit,SimVars.Mode)
+                SpineBoundingBox(S,SimVars.Unit,SimVars.Mode,Snaps = 1)
 
         else:
             for S in SynArr:
                 frame.set_status_message.setText(frame.set_status_message.text()+'.')
-                Mean,Area,Max,Min,RawIntDen,IntDen = MeasureShape(S, tiff_Arr[:,i,:,:], SimVars,Snaps)
-                S.max.append(Max)
-                S.min.append(Mean)
-                S.RawIntDen.append(RawIntDen)
-                S.IntDen.append(IntDen)
-                S.mean.append(Mean)
-                S.area.append(Area)
-                SpineBoundingBox(S,SimVars.Unit,SimVars.Mode)
+                for i in range(SimVars.Channels):
+                    Mean,Area,Max,Min,RawIntDen,IntDen = MeasureShape(S, tiff_Arr[:,i,:,:], SimVars,Snaps)
+                    S.max.append(Max)
+                    S.min.append(Min)
+                    S.RawIntDen.append(RawIntDen)
+                    S.IntDen.append(IntDen)
+                    S.mean.append(Mean)
+                    S.area.append(Area)
+                SpineBoundingBox(S,SimVars.Unit,SimVars.Mode,Snaps = Snaps)
     else:
-        if(SimVars.Mode=="Luminosity" or Snaps==1):
+        if(SimVars.Mode=="Luminosity"):
             for S in SynArr:
                 frame.set_status_message.setText(frame.set_status_message.text()+'.')
                 Mean,Area,Max,Min,RawIntDen,IntDen,local_bg = MeasureShape_and_BG(S, tiff_Arr[:,SimVars.frame.actual_channel,:,:], SimVars,Snaps)
@@ -479,18 +481,18 @@ def Measure(SynArr, tiff_Arr, SimVars,frame=None):
                 S.mean.append(Mean)
                 S.local_bg.append(local_bg)
                 S.area.append(Area[0])
-                SpineBoundingBox(S,SimVars.Unit,SimVars.Mode)
+                SpineBoundingBox(S,SimVars.Unit,SimVars.Mode,Snaps = 1)
         else:
             for S in SynArr:
                 frame.set_status_message.setText(frame.set_status_message.text()+'.')
                 Mean,Area,Max,Min,RawIntDen,IntDen = MeasureShape(S, tiff_Arr[:,SimVars.frame.actual_channel,:,:], SimVars,Snaps)
                 S.max.append(Max)
-                S.min.append(Mean)
+                S.min.append(Min)
                 S.RawIntDen.append(RawIntDen)
                 S.IntDen.append(IntDen)
                 S.mean.append(Mean)
                 S.area.append(Area)
-                SpineBoundingBox(S,SimVars.Unit,SimVars.Mode)
+                SpineBoundingBox(S,SimVars.Unit,SimVars.Mode,Snaps = Snaps)
 
 
 
@@ -501,45 +503,90 @@ def Measure(SynArr, tiff_Arr, SimVars,frame=None):
     NewNecks = []
     if(Snaps==1):
         for S in SynArr:
-            Intersection_pt, seg_idx = find_intersection(S.neck, S.points)
-            new_neck =np.vstack([Intersection_pt, S.neck[seg_idx+1:]])
-            S.neck_length = [GetLength(new_neck)*SimVars.Unit]
-            NewNecks.append(new_neck)
-    elif(SimVars.Mode=="Luminosity"):
-        if(SimVars.frame.local_shift):
-            temp_neck = []
-            for S in SynArr:
-                for n,s in zip(S.neck,S.shift):
-                    Intersection_pt, seg_idx = find_intersection(n, np.array(S.points)+ [s[0],s[1]])
-                    new_neck =np.vstack([Intersection_pt, n[seg_idx+1:]])
-                    S.neck_length.append(GetLength(new_neck)*SimVars.Unit)
-                    temp_neck.append(new_neck)
-                NewNecks.append(temp_neck)
-        else:   
-            for S in SynArr:
-                Intersection_pt, seg_idx = find_intersection(S.neck, S.points)
-                new_neck =np.vstack([Intersection_pt, S.neck[seg_idx+1:]])
+            if(S.type < 2):
+                try:
+                    if(np.array(S.neck).ndim>2):
+                        Intersection_pt, seg_idx = find_intersection(S.neck[0], S.points[0])
+                        new_neck =np.vstack([Intersection_pt, S.neck[0][seg_idx+1:]])[np.newaxis,:]
+                    else:
+                        Intersection_pt, seg_idx = find_intersection(S.neck, S.points)
+                        new_neck =np.vstack([Intersection_pt, S.neck[seg_idx+1:]])[np.newaxis,:]
+                except:
+                    new_neck = np.array(S.neck)
+
+
                 S.neck_length = [GetLength(new_neck)*SimVars.Unit]
                 NewNecks.append(new_neck)
+            else:
+                S.neck_length = [0]
+                NewNecks.append([])
+    elif(SimVars.Mode=="Luminosity"):
+        if(SimVars.frame.local_shift):
+            
+            for S in SynArr:
+                if(S.type < 2):
+                    nn = []
+                    for n,s in zip(S.neck,S.shift):
+                        try:
+                            Intersection_pt, seg_idx = find_intersection(n, np.array(S.points)+ [s[0],s[1]])
+                            new_neck =np.vstack([Intersection_pt, n[seg_idx+1:]])
+                        except:
+                            new_neck = np.array(n) 
+                        S.neck_length.append(GetLength(new_neck)*SimVars.Unit)
+                        nn.append(new_neck)
+
+                    NewNecks.append(nn)
+                else:
+                    S.neck_length = [0]
+                    NewNecks.append([])
+        else:   
+            for S in SynArr:
+                if(S.type < 2):
+                    try:
+                        Intersection_pt, seg_idx = find_intersection(S.neck, S.points)
+                        new_neck =np.vstack([Intersection_pt, S.neck[seg_idx+1:]])
+                    except:
+                        new_neck = np.array(S.neck)
+                    S.neck_length = [GetLength(new_neck)*SimVars.Unit]
+                    NewNecks.append(new_neck)
+                else:
+                    S.neck_length = [0]
+                    NewNecks.append([])
     elif(SimVars.Mode=="Area"):
         if(SimVars.frame.local_shift):
-            temp_neck = []
             for S in SynArr:
-                for n,s,p in zip(S.neck,S.shift,S.points):
-                    Intersection_pt, seg_idx = find_intersection(n, np.array(p)+ [s[0],s[1]])
-                    new_neck =np.vstack([Intersection_pt, n[seg_idx+1:]])
-                    S.neck_length.append(GetLength(new_neck)*SimVars.Unit)
-                    temp_neck.append(new_neck)
-            NewNecks.append(temp_neck)
+                if(S.type < 2):
+                    nn = []
+                    for n,s in zip(S.neck,S.shift):
+                        try:
+                            Intersection_pt, seg_idx = find_intersection(n, np.array(S.points)+ [s[0],s[1]])
+                            new_neck =np.vstack([Intersection_pt, n[seg_idx+1:]])
+                        except:
+                            new_neck = np.array(n) 
+                        S.neck_length.append(GetLength(new_neck)*SimVars.Unit)
+                        nn.append(new_neck)
+
+                    NewNecks.append(nn)
+                else:
+                    S.neck_length = [0]
+                    NewNecks.append([])
         else:
-            temp_neck = []
             for S in SynArr:
-                for n,p in zip(S.neck,S.points):
-                    Intersection_pt, seg_idx = find_intersection(n, p)
-                    new_neck =np.vstack([Intersection_pt, n[seg_idx+1:]])
-                    S.neck_length.append(GetLength(new_neck)*SimVars.Unit)
-                    temp_neck.append(new_neck)
-            NewNecks.append(temp_neck)
+                nn = []
+                if(S.type < 2):
+                    for n,p in zip(S.neck,S.points):
+                        try:
+                            Intersection_pt, seg_idx = find_intersection(n, p)
+                            new_neck =np.vstack([Intersection_pt, n[seg_idx+1:]])
+                        except:
+                            new_neck = np.array(n)
+                        S.neck_length.append(GetLength(new_neck)*SimVars.Unit)
+                        nn.append(new_neck)
+                    NewNecks.append(nn)
+                else:
+                    S.neck_length = [0]
+                    NewNecks.append([])
+
 
     neck_factor ="{:.1f}".format(frame.spine_neck_width_mult_slider.value()*0.1)
     frame.spine_neck_width_mult_counter.setText(neck_factor)
@@ -548,47 +595,61 @@ def Measure(SynArr, tiff_Arr, SimVars,frame=None):
     try:
         if hasattr(frame,"ContourLines"):
             for l in frame.ContourLines:
-                l.remove()
+                try:
+                    l.remove()
+                except:
+                    pass
             del frame.ContourLines
-    except:
+    except Exception as e:
         pass
-    frame.ContourLines = []
 
+    frame.ContourLines = []
     for N,S in zip(NewNecks,SynArr):
         if((SimVars.Mode == "Luminosity" and frame.local_shift) or (SimVars.Mode == "Area" and SimVars.multitime_flag)):
-            Contours = []
-            AvgWidth = []
-            AvgLum   = []
-            for i,n in enumerate(N):
-                n = np.round(n).astype(int)
-                bbmin = (max(np.min(n[:,1]) - 50, 0),max(np.min(n[:,0]) - 50, 0))
-                bbmax = (min(np.max(n[:,1]) + 50, tiff_Arr.shape[-2]),min(np.max(n[:,0]) + 50, tiff_Arr.shape[-1]))
-                n_shift = n-bbmin[::-1]
-                tiff_Arr_small = tiff_Arr[i,frame.actual_channel,bbmin[0]:bbmax[0], bbmin[1]:bbmax[1]]
-                c,w = FindNeckWidth(n_shift,tiff_Arr_small,S.neck_thresh[i],sigma = frame.spine_neck_sigma_slider.value(),width_factor = frame.spine_neck_width_mult_slider.value()*0.1)
-                contour = c[0] + bbmin[::-1]
-                AvgWidth.append(w*SimVars.Unit)
-                Contours.append(contour.squeeze())
-            S.neck_contours = Contours
-            S.neck_mean  = [Luminosity_from_contour(c,tiff_Arr[i]) for i,c in enumerate(S.neck_contours)]
-            S.neck_width = AvgWidth
-            line, = plt.plot(S.neck_contours[frame.actual_timestep][:, 0], S.neck_contours[frame.actual_timestep][:, 1], 'y')
-        else:
-            N = np.round(N).astype(int)
-            bbmin = (max(np.min(N[:,1]) - 50, 0),max(np.min(N[:,0]) - 50, 0))
-            bbmax = (min(np.max(N[:,1]) + 50, tiff_Arr.shape[-2]),min(np.max(N[:,0]) + 50, tiff_Arr.shape[-1]))
-            N_shift = N-bbmin[::-1]
-            tiff_Arr_small = tiff_Arr[frame.actual_timestep,frame.actual_channel,bbmin[0]:bbmax[0], bbmin[1]:bbmax[1]]
-            c,w = FindNeckWidth(N_shift,tiff_Arr_small,S.neck_thresh,sigma = frame.spine_neck_sigma_slider.value(),width_factor = frame.spine_neck_width_mult_slider.value()*0.1)
-            S.neck_contours = (c[0] + bbmin[::-1]).squeeze()
-            S.neck_width    = [w*SimVars.Unit]
-            if(SimVars.multitime_flag):
-                S.neck_mean  = [Luminosity_from_contour(S.neck_contours,tiff_Arr[i]) for i in range(SimVars.Snapshots)]
+            if(S.type < 2):
+                Contours = []
+                AvgWidth = []
+                AvgLum   = []
+                for i,n in enumerate(N):
+                    n = np.round(n).astype(int)
+                    bbmin = (max(np.min(n[:,1]) - 50, 0),max(np.min(n[:,0]) - 50, 0))
+                    bbmax = (min(np.max(n[:,1]) + 50, tiff_Arr.shape[-2]),min(np.max(n[:,0]) + 50, tiff_Arr.shape[-1]))
+                    n_shift = n-bbmin[::-1]
+                    tiff_Arr_small = tiff_Arr[i,frame.actual_channel,bbmin[0]:bbmax[0], bbmin[1]:bbmax[1]]
+                    c,w = FindNeckWidth(n_shift,tiff_Arr_small,S.neck_thresh[i],sigma = frame.spine_neck_sigma_slider.value(),width_factor = frame.spine_neck_width_mult_slider.value()*0.1)
+                    contour = c[0] + bbmin[::-1]
+                    AvgWidth.append(w*SimVars.Unit)
+                    Contours.append(contour.squeeze())
+                S.neck_contours = Contours
+                S.neck_mean  = np.array([Luminosity_from_contour(c,tiff_Arr[i]) for i,c in enumerate(S.neck_contours)]).T.tolist()
+                S.neck_width = AvgWidth
+                line, = plt.plot(S.neck_contours[frame.actual_timestep][:, 0], S.neck_contours[frame.actual_timestep][:, 1], 'y')
+                frame.ContourLines.append(line)
             else:
-                S.neck_mean  = Luminosity_from_contour(S.neck_contours,tiff_Arr[frame.actual_timestep])
-            line, = plt.plot(S.neck_contours[:, 0], S.neck_contours[:, 1], 'y')
-        frame.ContourLines.append(line)
+                S.neck_contours,S.neck_mean,S.neck_width = [],[],[]
+                frame.ContourLines.append([])
+        else:
+            if(S.type < 2):
+                N = np.round(N).astype(int)
+                if(N.ndim > 2):
+                    N = N[0]
+                bbmin = (max(np.min(N[:,1]) - 50, 0),max(np.min(N[:,0]) - 50, 0))
+                bbmax = (min(np.max(N[:,1]) + 50, tiff_Arr.shape[-2]),min(np.max(N[:,0]) + 50, tiff_Arr.shape[-1]))
+                N_shift = N-bbmin[::-1]
+                tiff_Arr_small = tiff_Arr[frame.actual_timestep,frame.actual_channel,bbmin[0]:bbmax[0], bbmin[1]:bbmax[1]]
 
+                c,w = FindNeckWidth(N_shift,tiff_Arr_small,S.neck_thresh,sigma = frame.spine_neck_sigma_slider.value(),width_factor = frame.spine_neck_width_mult_slider.value()*0.1)
+                S.neck_contours = (c[0] + bbmin[::-1]).squeeze()
+                S.neck_width    = [w*SimVars.Unit]
+                if(SimVars.multitime_flag):
+                    S.neck_mean  = np.array([Luminosity_from_contour(S.neck_contours,tiff_Arr[i]) for i in range(SimVars.Snapshots)]).T.tolist()
+                else:
+                    S.neck_mean  = np.array([Luminosity_from_contour(S.neck_contours,tiff_Arr[frame.actual_timestep])]).T.tolist()
+                line, = plt.plot(S.neck_contours[:, 0], S.neck_contours[:, 1], 'y')
+                frame.ContourLines.append(line)
+            else:
+                S.neck_contours,S.neck_mean,S.neck_width = [],[],[]
+                frame.ContourLines.append([])
 def Luminosity_from_contour(contour,image):
 
     height,width = image.shape[1:]
@@ -599,8 +660,7 @@ def Luminosity_from_contour(contour,image):
 
     mask_flat = path_contour.contains_points(points)
     mask = mask_flat.reshape((height, width))
-
-    return image[:,mask].mean(axis=-1).T.tolist()
+    return image[:,mask].mean(axis=-1)
 
 
 def MeasureShape(S, tiff_Arr, SimVars,Snapshots):
@@ -624,12 +684,42 @@ def MeasureShape(S, tiff_Arr, SimVars,Snapshots):
     Min  = []
     RawIntDen = []
     IntDen = []
-    for i in range(Snapshots):
-        try:
-            SynL = np.array(SynA[i]) + S.shift[i]
-        except:
-            SynL = np.array(SynA[i])
+    if(np.array(SynA).ndim == 3):
+        for i in range(Snapshots):
+            try:
+                SynL = np.array(SynA[i]) + S.shift[i]
+            except:
+                SynL = np.array(SynA[i])
+            SynL[:,0] = np.clip(SynL[:,0],0,tiff_Arr.shape[-1]-1)
+            SynL[:,1] = np.clip(SynL[:,1],0,tiff_Arr.shape[-2]-1)
+            
+            if SynL.ndim == 2:
+                mask = np.zeros(shape=tiff_Arr.shape[-2:], dtype=np.uint8)
+                c = SynL[:, 1]
+                r = SynL[:, 0]
+                rr, cc = polygon(r, c)
+                mask[cc, rr] = 1
+                props = regionprops(mask.astype(int))
+                try:
+                    roi  = tiff_Arr[i].astype(np.float64)
+                    roi[np.where(mask == 0)] = math.nan
+                    area_pix = np.sum(mask)
+                    area.append(int(area_pix) * SimVars.Unit**2)
+                    Max.append(int(np.nanmax(roi)))
+                    Min.append(int(np.nanmin(roi)))
+                    RawIntDen.append(int(np.nansum(roi)))
+                    IntDen.append(np.nansum(roi) * SimVars.Unit**2)
+                    Mean.append(np.nanmean(roi))
 
+                except Exception as ex:
+                    area.append(math.nan)
+                    Mean.append(math.nan)
+                    Max.append(math.nan)
+                    Min.append(math.nan)
+                    RawIntDen.append(math.nan)
+                    IntDen.append(math.nan)
+    else:
+        SynL = np.array(SynA)
         SynL[:,0] = np.clip(SynL[:,0],0,tiff_Arr.shape[-1]-1)
         SynL[:,1] = np.clip(SynL[:,1],0,tiff_Arr.shape[-2]-1)
         
@@ -658,6 +748,8 @@ def MeasureShape(S, tiff_Arr, SimVars,Snapshots):
                 Min.append(math.nan)
                 RawIntDen.append(math.nan)
                 IntDen.append(math.nan)
+
+
 
     return Mean,area,Max,Min,RawIntDen,IntDen
 
@@ -736,7 +828,7 @@ def MeasureShape_and_BG(S, tiff_Arr, SimVars, Snapshots):
 
     return Mean,area,Max,Min,RawIntDen,IntDen,local_bg
 
-def SpineBoundingBox(S,Unit,Mode):
+def SpineBoundingBox(S,Unit,Mode,Snaps):
     """
     Rotate coordinates (x, y) by angle `theta` (radians).
     (x, y) can be arrays. 
@@ -747,7 +839,7 @@ def SpineBoundingBox(S,Unit,Mode):
     cos_t = np.cos(-S.Orientation)
     sin_t = np.sin(-S.Orientation)
     
-    if(Mode=="Luminosity"):
+    if(Snaps == 1):
         # Apply the rotation
         x_r = cos_t * np.array(S.points)[:,0] - sin_t * np.array(S.points)[:,1]
         y_r = sin_t * np.array(S.points)[:,0] + cos_t * np.array(S.points)[:,1]
@@ -756,7 +848,7 @@ def SpineBoundingBox(S,Unit,Mode):
         ymin, ymax = y_r.min(), y_r.max()
         
         S.widths.append([(xmax - xmin)*Unit,(ymax - ymin)*Unit])
-    elif(Mode=="Area"):
+    else:
         for pts in S.points:
                 # Apply the rotation
                 x_r = cos_t * np.array(pts)[:,0] - sin_t * np.array(pts)[:,1]

App/PathFinding.py
@@ -204,7 +204,7 @@ def GetAllpointsonPath(xys):
 
 
 def medial_axis_path(
-    mesh: np.ndarray, start: np.ndarray, end: np.ndarray, scale=1
+    mesh: np.ndarray, start: np.ndarray, end: np.ndarray, scale=1,verbose = False
 ) -> tuple[np.ndarray, float]:
     """
     converts image to graph module networkx, computes shortest path between two points where
@@ -304,7 +304,7 @@ def medial_axis_path(
         length = GetLength(coords)
         return coords, length * scale
     else:
-        print("points were not eligible, change threshold and start again!")
+        if verbose: print("points were not eligible, change threshold and start again!")
 
 def GetLength(coords):
     length = 0

App/Spine.py
@@ -40,6 +40,7 @@ class Synapse(object):
         self.neck_thresh = neck_thresh
 
         self.neck_contours = []
+        self.neck_length   = []
 
 class Spine_Marker:
 
@@ -165,7 +166,10 @@ class Spine_Marker:
                         self.SimVars.scores_NN = np.delete(nn_scores[nn_scores>self.SimVars.frame.ml_confidence_slider.value()/10],index)
                         self.SimVars.flags_NN = np.delete(nn_flags[nn_scores>self.SimVars.frame.ml_confidence_slider.value()/10],index)
             if len(self.points) == 0:
-                self.scatter.set_visible(False)
+                try:
+                    self.scatter.set_visible(False)
+                except:
+                    pass
             self.draw_points()
 
     def _on_key_release(self, event):
@@ -200,7 +204,6 @@ class Spine_Marker:
         """
         zoom_flag = self.SimVars.frame.mpl.toolbox.mode == "zoom rect"
         pan_flag = self.SimVars.frame.mpl.toolbox.mode == "pan/zoom"
-
         if zoom_flag or pan_flag or not event.inaxes:
             pass
         else:
@@ -210,6 +213,8 @@ class Spine_Marker:
                 self.scores = np.array([1])
                 if(self.shift_is_held):
                     self.flags = np.array([1])
+                elif(self.control_is_held):
+                    self.flags = np.array([2])
                 else:
                     self.flags = np.array([0])
             else:

App/SynapseFuncs.py
@@ -24,6 +24,7 @@ from .PathFinding import (
 from scipy.ndimage import gaussian_filter1d, gaussian_filter
 from scipy.ndimage import distance_transform_edt
 
+
 def SpineShift(tiff_Arr_small):
 
     """
@@ -345,14 +346,13 @@ def FindNeck(SpineC,DendProj,image,DendArr):
     distances = np.array([np.min(np.linalg.norm(t-DendArr,axis=-1)) for t in shifted_path])
     cutoff = np.argmin(distances)
 
-    return shifted_path[:cutoff].tolist(),neck_thresh
+    return shifted_path[:cutoff].tolist(),neck_thresh.astype(np.float64)
 
 def FindNeckWidth(neck_path,image, thresh, max_neighbours: int = 1, sigma: int = 10, width_factor: int=0.1):
+
     all_points = GetAllpointsonPath(np.round(neck_path).astype(int))
 
-    gaussian_x = gaussian_filter1d(
-        input=all_points[:, 1], mode="nearest", sigma=sigma
-    ).astype(int)
+    gaussian_x = gaussian_filter1d(        input=all_points[:, 1], mode="nearest", sigma=sigma    ).astype(int)
     gaussian_y = gaussian_filter1d(
         input=all_points[:, 0], mode="nearest", sigma=sigma
     ).astype(int)
@@ -363,7 +363,6 @@ def FindNeckWidth(neck_path,image, thresh, max_neighbours: int = 1, sigma: int =
         median_thresh = median >= thresh
     else:
         median_thresh = median >= np.mean(median)
-
     width_arr, degrees = getWidthnew(
         median_thresh,
         smoothed_all_pts,
@@ -453,7 +452,8 @@ class NumpyEncoder(json.JSONEncoder):
         if isinstance(obj, np.integer):
             return int(obj)
         return json.JSONEncoder.default(self, obj)
-
+def to_list(val):
+    return val.tolist() if hasattr(val, 'tolist') else val
 
 def SaveSynDict(SynArr, Dir, Mode,xLims):
 
@@ -477,25 +477,36 @@ def SaveSynDict(SynArr, Dir, Mode,xLims):
         Lims = np.array([0,0])
     else:
         Lims = np.array([xLims[0][0],xLims[1][0]])
+   
+    # For each object, subtract Lims where possible and convert attributes to lists.
     for S in modifiedSynArr:
-        try:
-            S.points   = S.points   - Lims
-            S.location = S.location - Lims
-            S.bgloc    = S.bgloc    - Lims
-        except Exception as e:
-            pass
-        try:
-            S.points   = [arr.tolist() for arr in S.points]
-            S.location = S.location.tolist()
-            S.distance_to_Dend = S.distance_to_Dend.tolist()
-            S.bgloc    = S.bgloc.tolist()
-        except:
-            pass
-        try:
-            S.bgloc = S.bgloc.tolist()
-            S.neck    = S.neck.tolist()
-        except:
-            pass
+        # Subtract Lims from attributes if they exist.
+        for attr in ['points', 'location', 'bgloc']:
+            if hasattr(S, attr):
+                try:
+                    # Subtraction should work if the attribute is a NumPy array or similar.
+                    setattr(S, attr, getattr(S, attr) - Lims)
+                except Exception as e:
+                    pass
+        
+        # Convert attributes to lists.
+        # For 'points', we handle nested conversion if needed.
+        if hasattr(S, 'points'):
+            try:
+                if isinstance(S.points, list):
+                    S.points = [to_list(item) for item in S.points]
+                else:
+                    S.points = to_list(S.points)
+            except Exception as e:
+                pass
+        
+        for attr in ['location', 'distance_to_Dend', 'bgloc', 'neck', 'neck_mean']:
+            if hasattr(S, attr):
+                try:
+                    setattr(S, attr, to_list(getattr(S, attr)))
+                except Exception as e:
+                    pass
+
     if Mode == "Area":
         with open(Dir + "Synapse_a.json", "w") as fp:
             json.dump([vars(S) for S in modifiedSynArr], fp, indent=4,cls=NumpyEncoder)
@@ -619,6 +630,12 @@ def SpineSave_csv(Dir,Spine_Arr,nChans,nSnaps,Mode,xLims):
     Returns:
         None
     """   
+    OnlySoma = False
+    if(np.all([S.type == 2 for S in Spine_Arr])):
+        OnlySoma = True
+    else:
+        FirstNonSoma = np.argmin([S.type == 2 for S in Spine_Arr])
+
     if(len(xLims[0])==0):
         Lims = np.array(0)
     else:
@@ -626,65 +643,130 @@ def SpineSave_csv(Dir,Spine_Arr,nChans,nSnaps,Mode,xLims):
     if(Mode=='Luminosity'):
         custom_header =(['', 'type','location','bgloc','area','distance','closest_Dend','Max, dist to Dend',
             'Center dist to dend','Min, dist to Dend','Widths'] + 
-        ['Timestep '+ str(i+1) +' (neck length)' for i in range(len(Spine_Arr[0].neck_length))] +
-        ['Timestep '+ str(i+1) +' (neck width)' for i in range(len(Spine_Arr[0].neck_width))] +
         ['Timestep '+ str(i) +' (mean)' for i in range(1,nSnaps+1)] +
         ['Timestep '+ str(i) +' (min)' for i in range(1,nSnaps+1)] +
         ['Timestep '+ str(i) +' (max)' for i in range(1,nSnaps+1)] +
         ['Timestep '+ str(i) +' (RawIntDen)' for i in range(1,nSnaps+1)] +
         ['Timestep '+ str(i) +' (IntDen)' for i in range(1,nSnaps+1)] +
-        ['Timestep '+ str(i) +' (bg mean)' for i in range(1,nSnaps+1)] + 
-        ['Timestep '+ str(i) +' (neck mean)' for i in range(1,nSnaps+1)]) 
+        ['Timestep '+ str(i) +' (bg mean)' for i in range(1,nSnaps+1)])
+        if(not OnlySoma):
+            custom_header +=['Timestep '+ str(i+1) +' (neck length)' for i in range(len(Spine_Arr[FirstNonSoma].neck_length))] + ['Timestep '+ str(i+1) +' (neck width)' for i in range(len(Spine_Arr[FirstNonSoma].neck_width))] + ['Timestep '+ str(i) +' (neck mean)' for i in range(1,nSnaps+1)]
         for c in range(nChans):
             csv_file_path = Dir+'Synapse_l_Channel_' + str(c)+'.csv'
             with open(csv_file_path, 'w', newline='') as file:
                 writer = csv.writer(file)
                 writer.writerow(custom_header) 
-                for i,s in enumerate(Spine_Arr):
-                    row = ['Spine: '+str(i),s.type,
-                                     str(s.location-Lims),str(s.bgloc-Lims),s.area,s.distance,s.closest_Dend
-                                     ]+[str(d) for d in s.distance_to_Dend] + [str(s.widths)] + [str(n) for n in s.neck_length] +[str(n) for n in s.neck_width]
-                                     
+                if(OnlySoma):
+                    for i,s in enumerate(Spine_Arr):
+                        row = ['Spine: '+str(i),s.type,
+                                         str(s.location-Lims),str(s.bgloc-Lims),s.area,s.distance,s.closest_Dend
+                                         ]+[str(d) for d in s.distance_to_Dend] + [str(s.widths)]
+                        
 
-                    row.extend(s.mean[c])
-                    row.extend(s.min[c])
-                    row.extend(s.max[c])
-                    row.extend(s.RawIntDen[c])
-                    row.extend(s.IntDen[c])
-                    row.extend(s.local_bg[c])
-                    row.extend(s.neck_mean[c])
-                    writer.writerow(row)   
-    else:
+                        row.extend(s.mean[c])
+                        row.extend(s.min[c])
+                        row.extend(s.max[c])
+                        row.extend(s.RawIntDen[c])
+                        row.extend(s.IntDen[c])
+                        row.extend(s.local_bg[c])
+                        writer.writerow(row)
+                else:
+                    for i,s in enumerate(Spine_Arr):
+
+                        if(s.type == 2):
+                            row = ['Spine: '+str(i),s.type,
+                                             str(s.location-Lims),str(s.bgloc-Lims),s.area,s.distance,s.closest_Dend
+                                             ]+[str(d) for d in s.distance_to_Dend] + [str(s.widths)]
+                            
+
+                            row.extend(s.mean[c])
+                            row.extend(s.min[c])
+                            row.extend(s.max[c])
+                            row.extend(s.RawIntDen[c])
+                            row.extend(s.IntDen[c])
+                            row.extend(s.local_bg[c])
+                            row += ['' for n in Spine_Arr[FirstNonSoma].neck_length] +['' for n in Spine_Arr[FirstNonSoma].neck_width]
+                            row.extend(np.zeros_like(Spine_Arr[FirstNonSoma].neck_mean[c]))
+                            writer.writerow(row)
+                        else:
+                            row = ['Spine: '+str(i),s.type,
+
+                                         str(s.location-Lims),str(s.bgloc-Lims),s.area,s.distance,s.closest_Dend
+                                         ]+[str(d) for d in s.distance_to_Dend] + [str(s.widths)]
+                        
+
+                            row.extend(s.mean[c])
+                            row.extend(s.min[c])
+                            row.extend(s.max[c])
+                            row.extend(s.RawIntDen[c])
+                            row.extend(s.IntDen[c])
+                            row.extend(s.local_bg[c])
+                            row += [str(n) for n in s.neck_length] +[str(n) for n in s.neck_width]
+                            row.extend(s.neck_mean[c])
+                            writer.writerow(row)
+
+    else: # TODO: Adapt this one like the one above! - Puncta analysis!
         custom_header =(['', 'type','location','distance','closest_Dend','Max. dist to Dend',
             'Center dist to dend','Min. dist to Dend'] + 
-        ['Timestep '+ str(i+1) +' (neck length)' for i in range(len(Spine_Arr[0].neck_length))] +
-        ['Timestep '+ str(i+1) +' (neck width)' for i in range(len(Spine_Arr[0].neck_width))] +
         ['Timestep '+ str(i) +' (area)' for i in range(1,nSnaps+1)] +  
         ['Timestep '+ str(i) +' (mean)' for i in range(1,nSnaps+1)] +
         ['Timestep '+ str(i) +' (min)' for i in range(1,nSnaps+1)] +
         ['Timestep '+ str(i) +' (max)' for i in range(1,nSnaps+1)] +
         ['Timestep '+ str(i) +' (RawIntDen)' for i in range(1,nSnaps+1)] +
         ['Timestep '+ str(i) +' (IntDen)' for i in range(1,nSnaps+1)] + 
-        ['Timestep '+ str(i) +' (widths)' for i in range(1,nSnaps+1)] +
-        ['Timestep '+ str(i) +' (neck mean)' for i in range(1,nSnaps+1)]) 
+        ['Timestep '+ str(i) +' (Widths)' for i in range(1,nSnaps+1)])
+        if(not OnlySoma):
+            custom_header +=['Timestep '+ str(i+1) +' (neck length)' for i in range(len(Spine_Arr[FirstNonSoma].neck_length))] + ['Timestep '+ str(i+1) +' (neck width)' for i in range(len(Spine_Arr[FirstNonSoma].neck_width))] + ['Timestep '+ str(i) +' (neck mean)' for i in range(1,nSnaps+1)]
+       
         for c in range(nChans):
             csv_file_path = Dir+'Synapse_a_Channel_' + str(c)+'.csv'
             with open(csv_file_path, 'w', newline='') as file:
                 writer = csv.writer(file)
                 writer.writerow(custom_header) 
-                for i,s in enumerate(Spine_Arr):
-                    row = ['Spine: '+str(i),s.type,
-                                     str(s.location-Lims),s.distance,s.closest_Dend
-                                     ]+[str(d) for d in s.distance_to_Dend] + [str(n) for n in s.neck_length] +[str(n) for n in s.neck_width]
-                    row.extend(s.area[c])
-                    row.extend(s.mean[c])
-                    row.extend(s.min[c])
-                    row.extend(s.max[c])
-                    row.extend(s.RawIntDen[c])
-                    row.extend(s.IntDen[c])
-                    row.extend([str(w) for w in s.widths])
-                    row.extend(s.neck_mean[c])
-                    writer.writerow(row)   
+                if(OnlySoma):
+                    for i,s in enumerate(Spine_Arr):
+                        row = ['Spine: '+str(i),s.type,
+                                         str(s.location-Lims),s.distance,s.closest_Dend
+                                         ]+[str(d) for d in s.distance_to_Dend]
+                        row.extend(s.area[c])
+                        row.extend(s.mean[c])
+                        row.extend(s.min[c])
+                        row.extend(s.max[c])
+                        row.extend(s.RawIntDen[c])
+                        row.extend(s.IntDen[c])
+                        row.extend([str(w) for w in s.widths])
+                        writer.writerow(row)   
+                else:
+                    for i,s in enumerate(Spine_Arr):
+                        if(s.type == 2):
+                            row = ['Spine: '+str(i),s.type,
+                                             str(s.location-Lims),s.distance,s.closest_Dend
+                                             ]+[str(d) for d in s.distance_to_Dend]
+                            
+                            row.extend(s.area[c])
+                            row.extend(s.mean[c])
+                            row.extend(s.min[c])
+                            row.extend(s.max[c])
+                            row.extend(s.RawIntDen[c])
+                            row.extend(s.IntDen[c])
+                            row.extend([str(w) for w in s.widths])
+                            row += ['' for n in Spine_Arr[FirstNonSoma].neck_length] +['' for n in Spine_Arr[FirstNonSoma].neck_width]
+                            row.extend(np.zeros_like(Spine_Arr[FirstNonSoma].neck_mean[c]))
+                            writer.writerow(row)
+                        else:
+                            row = ['Spine: '+str(i),s.type,
+                                         str(s.location-Lims),s.distance,s.closest_Dend
+                                         ]+[str(d) for d in s.distance_to_Dend]
+                            row.extend(s.area[c])
+                            row.extend(s.mean[c])
+                            row.extend(s.min[c])
+                            row.extend(s.max[c])
+                            row.extend(s.RawIntDen[c])
+                            row.extend(s.IntDen[c])
+                            row.extend([str(w) for w in s.widths])
+                            row += [str(n) for n in s.neck_length] +[str(n) for n in s.neck_width]
+                            row.extend(s.neck_mean[c])
+                            writer.writerow(row)
 
 def SpineSave_imj(Dir,Spine_Arr):
     os.mkdir(path=Dir+'ImageJ/')
@@ -700,13 +782,14 @@ def SpineSave_imj(Dir,Spine_Arr):
             roi = rf.ImagejRoi.frompoints(pts)
             roi.roitype = rf.ROI_TYPE.POLYGON
             roi.tofile(Dir2+'Spine_'+str(i)+'.roi')
-        necks = S.neck_contours
-        if(len(necks[0])>2):
-            for j,p in enumerate(necks):
-                roi = rf.ImagejRoi.frompoints(p)
+        if(S.type < 2):
+            necks = S.neck_contours
+            if(len(necks[0])>2):
+                for j,p in enumerate(necks):
+                    roi = rf.ImagejRoi.frompoints(p)
+                    roi.roitype = rf.ROI_TYPE.POLYGON
+                    roi.tofile(Dir2+'Spine_neck_'+str(i)+'_t'+str(j)+'.roi')
+            else:
+                roi = rf.ImagejRoi.frompoints(necks)
                 roi.roitype = rf.ROI_TYPE.POLYGON
-                roi.tofile(Dir2+'Spine_neck_'+str(i)+'_t'+str(j)+'.roi')
-        else:
-            roi = rf.ImagejRoi.frompoints(necks)
-            roi.roitype = rf.ROI_TYPE.POLYGON
-            roi.tofile(Dir2+'Spine_neck_'+str(i)+'.roi')
+                roi.tofile(Dir2+'Spine_neck_'+str(i)+'.roi')

App/Utility.py
@@ -247,8 +247,11 @@ def getWidthnew(img, all_ps, sigma, max_neighbours, width_factor = 1):
     for i in range(len(width_arr) - 1):
         if width_arr[i + 1] > width_arr[i] + max_neighbours:
             width_arr[i + 1] = width_arr[i]
-
-    width_arr[-1] = width_arr[-2]
+    try:
+        width_arr[-1] = width_arr[-2]
+    except Exception as e:
+       # Print the error message associated with the exception
+        pass
     ##########################
     reverted = np.flip(width_arr, axis=0)
     for i in range(len(reverted) - 1):
@@ -565,27 +568,34 @@ def averager(arr: np.array, n: int) -> tuple[np.array, np.array]:
             averages.append(res)
         return np.array(averages), boarders
 
-
-def curvature_dependent_sampling(arr: np.array, pixels_per_intervall: int) -> np.array:
+def curvature_dependent_sampling(arr: np.array, pixels_per_intervall: int, min_points: int = 10) -> np.array:
     """
-    function to calculate the points for piecewise linear approximation of the medial axis
-    to reduce the medial axis path to make it presentable in the GUI
-    high curvature means high sampling and vice versa
-    :param arr: curvature per pixel of dendrite path
-    :param pixels_per_intervall: size of interval for average
-    :return: indices of the curvature sampled points
+    Calculate points for a piecewise linear approximation of the medial axis
+    based on curvature-dependent sampling. High curvature regions will have more sampling.
+    
+    :param arr: Curvature per pixel along the dendrite path.
+    :param pixels_per_intervall: Interval size for averaging.
+    :param min_points: Minimum number of points to guarantee in the final sampling.
+    :return: A tuple (sampling, boarders, aver) where:
+             - sampling: indices of the sampled points,
+             - boarders: interval borders from the averaging,
+             - aver: the averaged curvature values.
     """
-    aver, boarders = averager(arr, pixels_per_intervall)  # sub averaging
-    res = list(
-        map(curvature_eval, aver)
-    )  # evaluate mean curvature to get number of points in a sample
+    aver, boarders = averager(arr, pixels_per_intervall)  # sub-averaging
+    res = list(map(curvature_eval, aver))  # evaluate curvature to get number of points per sample
 
     sampling = np.array([])
     for i in range(len(aver)):
+        # For each interval, generate points based on the computed number plus one.
         intervall = np.linspace(boarders[i], boarders[i + 1], res[i] + 1)
+        # Exclude the last point to avoid duplicates (except for the final interval)
         sampling = np.append(sampling, intervall[:-2])
     sampling = np.append(sampling, boarders[-1])
     sampling = sampling.astype(int)
+
+    # Ensure a minimum number of points.
+    if sampling.size < min_points:
+        sampling = np.linspace(boarders[0], boarders[-1], min_points, dtype=int)
 
     return sampling, boarders, aver
 